67a53d0ee808fab14a1bd6e0432ee2118a2c96f4
sobuj <sobujahmed124@gmail.com>
2023-03-26 23:36:42 +0600

    desturcturing

---
+++ destructuring.js
@@ -0,0 +1,15 @@
+// destructuring array
+
+const numbers = [44, 54];
+// method 1
+const x = numbers[0];
+const y = numbers[1];
+// console.log(x, y);
+
+// method 2
+//const [first, second] = [44, 54];
+// console.log(first, second);
+
+// method 3
+const [first, second] = numbers;
+console.log(first, second);
