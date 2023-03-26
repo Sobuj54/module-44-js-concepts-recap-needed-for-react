c65799d438e2b63a7ffb834b21fad25252ee536c
sobuj <sobujahmed124@gmail.com>
2023-03-26 23:59:17 +0600

    object destructuring

--- destructuring.js
+++ destructuring.js
@@ -13,3 +13,28 @@ const y = numbers[1];
 // method 3
 const [first, second] = numbers;
 console.log(first, second);
+
+// object destructuring
+const { name, age } = { name: "sobuj", age: 22 };
+console.log(name, age);
+// another method
+const employee = {
+  ide: "vs code",
+  designation: "developer",
+  machine: "dell",
+  specification: {
+    height: 6,
+    weight: 64,
+    address: {
+      home: "tangail",
+      division: "dhaka",
+    },
+  },
+};
+const { ide, machine } = employee;
+// console.log(ide, machine);
+const { height, weight } = employee.specification;
+// console.log(height, weight);
+// this is know as optional chaining
+const { home } = employee?.specification?.address;
+console.log(home, division);
