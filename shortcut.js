4971203e438ff27b9ec5f59b2110c27b8c593f1a
sobuj <sobujahmed124@gmail.com>
2023-03-27 16:59:09 +0600

    num to string and vice varsa

---
+++ shortcut.js
@@ -0,0 +1,11 @@
+let num = 52;
+console.log(num);
+// shortcut to convert inter to stirng
+let numString = num + "";
+console.log(numString);
+
+// shortcut to convert string to number
+let string = "22";
+console.log(string);
+let stringNum = +string;
+console.log(stringNum);
