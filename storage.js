ce8cd38c0078ce99d4171c61e23722ebd3242a80
sobuj <sobujahmed124@gmail.com>
2023-03-27 19:28:52 +0600

    local storage basic

---
+++ storage.js
@@ -0,0 +1,6 @@
+const addToLocalStorage = () => {
+  const idInput = document.getElementById("storage-id");
+  const id = idInput.value;
+  const valueInput = document.getElementById("storage-value");
+  const value = valueInput.value;
+};
